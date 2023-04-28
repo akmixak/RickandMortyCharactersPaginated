5b0ceba08cfb32450fdc25bcaed8383149842db2
ankit.kumar <ankit.kumar@sprinklr.com>
2023-04-28 16:05:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
 
-function App() {
+import { useQuery, gql, NetworkStatus } from "@apollo/client";
+
+let cache;
+
+const GET_CHARACTERS = gql`
+  query getCharctersList($page: Int) {
+    characters(page: $page) {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+function CharactersList() {
+  const { data, loading, error, fetchMore, networkStatus } = useQuery(
+    GET_CHARACTERS,
+    {
+      variables: {
+        page: 1,
+      },
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+  const currStatus = React.useRef(null);
+  //console.log(networkStatus);
+  const page = React.useRef(1);
+  const [fetchMoreLoading, setFetchMoreLoading] = React.useState(false);
+  // console.log(loading, networkStatus);
+  const handler = React.useCallback(() => {
+    // console.log(loading, "loading inside handler");
+    if (!loading && currStatus.current !== "refetch") {
+      currStatus.current = "refetch";
+      fetchMore({
+        variables: {
+          page: page.current + 1,
+        },
+        updateQuery(prv, { fetchMoreResult: fetchResult }) {
+          if (!fetchResult) {
+            return prv;
+          }
+          return {
+            characters: {
+              ...prv.characters,
+              results: [
+                ...prv.characters.results,
+                ...fetchResult.characters.results,
+              ],
+            },
+          };
+        },
+      }).then(() => (currStatus.current = null));
+      //currStatus.current = "null";
+      page.current = page.current + 1;
+    }
+  }, [data]);
+
+  const onScrollHandler = React.useCallback(() => {
+    let cords = document.body.getBoundingClientRect();
+    console.log(cords.bottom - window.innerHeight);
+    if (Math.floor(cords.bottom - window.innerHeight) <= 0) {
+      handler();
+    }
+  }, [data, handler, fetchMoreLoading]);
+  React.useEffect(() => {
+    console.log(loading, "loading inside effect");
+    window.addEventListener("scroll", onScrollHandler);
+    //window.addEventListener("scroll", onScrollHandler);
+    return () => {
+      window.removeEventListener("scroll", onScrollHandler);
+    };
+  }, [onScrollHandler]);
+
+  if (loading && networkStatus !== NetworkStatus.fetchMore) return "Loading...";
+  if (error) return <div>{error.message}</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.characters.results.map((character) => {
+          return (
+            <li
+              key={character.id}
+              style={{ margin: "auto", width: "min-content" }}
+            >
+              <div>{character.id}</div>
+              <div>{character.name}</div>
+              <div>
+                <img src={character.image} />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <button onClick={handler}>Load More</button>; */}
+      {loading ? "loading" : null}
+      <div style={{ height: "150px", width: "100%" }}></div>
+      <div style={{ height: "150px", width: "100%" }}></div>
+    </div>
+  );
+  return null;
+}
+
+// For Fun Only
+
+function ForFunOnly() {}
+
+function App({ client }) {
+  cache = client.cache;
+  return (
+    <div>
+      <CharactersList />
+      {/* <ForFunOnly /> */}
     </div>
   );
 }
